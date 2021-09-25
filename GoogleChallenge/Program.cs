744277f7cdf9ffe7d76d7a8764adc26ad882976e
Nathan Foreman <nathanforeman746@gmail.com>
2021-09-25 12:56:16 -0500

    initial page

---
+++ GoogleChallenge/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "Supercalifragilisticexpialidocious";
+
+            foreach (char letter in word)
+            {
+                Console.WriteLine(letter);
+            }
+
+            foreach (char letter in word)
+            {
+                if (letter == 'i')
+                {
+                    Console.WriteLine(letter);
+                }
+                else if(letter == 'l')
+                {
+                    Console.WriteLine(letter);
+                }
+                else
+                {
+                    Console.WriteLine("Not an i");
+                }
+                
+            }
+
+            Console.WriteLine();
+
+            int count = 0;
+            foreach(char letter in word)
+            {
+                count++;
+            }
+            Console.WriteLine(count);
+
+            Console.WriteLine(word.Length);
+
+            Console.ReadKey();
+        }
+    }
+}
